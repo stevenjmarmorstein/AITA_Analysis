0afdfe92d7dbd72118372c238aa34951769bc87c
Steven Marmorstein <stevenjmarmorstein@gmail.com>
2019-09-24 15:11:34 -0700

    Reorganized code

--- classifiers.py
+++ classifiers.py
@@ -13,23 +13,8 @@ from sklearn.metrics import classification_report
 
 
 def main(*args):
-    pos_labels = ["NTA", "YTA", "NAH", "ESH"]
 
-    raw_data = get_data()
-    cleaned_inputs = clean_posts(raw_data['title'], raw_data['body'])
-    labels = raw_data['label']
-
-    # should remove INFO posts here
-    data_pairs = [(cleaned_inputs[i], labels[i]) for i in range(len(labels)) if labels[i] != "INFO"]
-
-    # Dividing the data 65/35 into train and test
-    random.shuffle(data_pairs)
-    x_train = [data_pairs[i][0] for i in range(round(len(data_pairs)*.65))]
-    y_train = [data_pairs[i][1] for i in range(round(len(data_pairs)*.65))]
-    x_test = [data_pairs[i][0] for i in range((round(len(data_pairs)*.65)), len(data_pairs))]
-    y_test = [data_pairs[i][1] for i in range((round(len(data_pairs)*.65)), len(data_pairs))]
-
-
+    x_train, y_train, x_test, y_test = get_data_sets()
 
     logreg = Pipeline([('vect', CountVectorizer()),
                 ('tfidf', TfidfTransformer()),
@@ -42,6 +27,32 @@ def main(*args):
     print('accuracy %s' % accuracy_score(y_predicted, y_test))
     print(classification_report(y_test, y_predicted))
 
+    # Makes a sound to indicate that the program is done:
+    print("\a")
+
+def get_data_sets():
+    """
+    Returns a 4-tuple containing the (training inputs, the training outputs, the
+    test inputs, and the test outputs).
+    """
+    raw_data = get_raw_data()
+    cleaned_inputs = clean_posts(raw_data['title'], raw_data['body'])
+    labels = raw_data['label']
+
+    train_proportion = .65
+
+    # should remove INFO posts here
+    data_pairs = [(cleaned_inputs[i], labels[i]) for i in range(len(labels)) if labels[i] != "INFO" and "removed" not in cleaned_inputs[i]]
+
+    # Dividing the data 65/35 into train and test
+    random.shuffle(data_pairs)
+    x_train = [data_pairs[i][0] for i in range(round(len(data_pairs)*train_proportion))]
+    y_train = [data_pairs[i][1] for i in range(round(len(data_pairs)*train_proportion))]
+    x_test = [data_pairs[i][0] for i in range((round(len(data_pairs)*train_proportion)), len(data_pairs))]
+    y_test = [data_pairs[i][1] for i in range((round(len(data_pairs)*train_proportion)), len(data_pairs))]
+
+    return (x_train, y_train, x_test, y_test)
+
 
 def clean_posts(titles, bodies):
     """
@@ -50,7 +61,6 @@ def clean_posts(titles, bodies):
     the words from each element of the pair have been combined into one list.
     """
     # TODO: replace punctuation with spaces
-    # remove [removed] posts
     stopwords_set = set(nltk.corpus.stopwords.words('english'))
     posts = []
     for i in range(len(titles)):
@@ -65,7 +75,7 @@ def clean_posts(titles, bodies):
 
 
 
-def get_data():
+def get_raw_data():
     """
     Reads the data from data.json and returns the dict that should be stored
     there. Data should be a dictionary with three string keys: label, title,
